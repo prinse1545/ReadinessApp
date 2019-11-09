3cc122c6627316927510c7648d7f9aa6e74eaa66
prinse1545 <philipp.msrivastava@gmail.com>
2019-11-08 20:29:58 -0500

    started server

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/query')
+const Mutation = require('./resolvers/mutation')
+const User = require('./resolvers/user')
+const Subscription = require('./resolvers/subscription')
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+
+}
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+})
+
+server.start(() => console.log("Server is running on http://localhost:4000"))
