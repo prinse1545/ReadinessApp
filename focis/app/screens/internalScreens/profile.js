308d1b1cf7be957f76464307973a783fd7078307
prinse1545 <philipp.msrivastava@gmail.com>
2019-11-11 16:12:54 -0500

    added chart

--- focis/app/screens/internalScreens/profile.js
+++ focis/app/screens/internalScreens/profile.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
   StyleSheet,
   Image,
   TouchableOpacity,
+  Dimensions
 } from 'react-native';
 
 import { Icon } from 'react-native-elements';
 
+import { LineChart } from "react-native-chart-kit";
+
 const Profile = ({navigation}) => {
+
+  const [questions, setQuestions] = useState([])
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
@@ -18,6 +24,56 @@ const Profile = ({navigation}) => {
         </TouchableOpacity>
         <Text style={styles.name}>First Name Last Name </Text>
       </View>
+      <View style={styles.questionsContainer}>
+        {
+          questions.length == 0 ?
+          <Text style={styles.noQuestionText}> No Questions at this Time</Text>
+          :
+          null
+        }
+      </View>
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel={"$"}
+        yAxisSuffix={"k"}
+        chartConfig={{
+          backgroundColor: '#035096',
+          backgroundGradientFrom: '#1E90FF',
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
     </View>
   );
 }
@@ -26,7 +82,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#035096',
-    paddingTop: '13%'
+    paddingTop: '13%',
+    alignItems: 'center'
   },
   name: {
     fontFamily: 'Avenir',
@@ -47,8 +104,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderBottomColor: "#fff",
     paddingBottom: '4%',
-    borderBottomWidth: 2
+    borderBottomWidth: 2,
+    width: '100%'
+  },
+  questionsContainer: {
+    height: '20%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  noQuestionText: {
+    fontFamily: "Avenir",
+    fontSize: 22,
+    color: "#fff"
   }
+
 })
 
 export default Profile;
