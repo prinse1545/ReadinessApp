6836ae0165344e08c50cb0c8597b556e8f5b5724
prinse1545 <philipp.msrivastava@gmail.com>
2019-11-11 11:55:05 -0500

    deleted token.js

--- focis/app/config/token.js
+++
@@ -1,7 +0,0 @@
-import AsyncStorage from '@react-native-community/async-storage';
-
-const AUTH_TOKEN = 'focis-auth-token';
-
-export const getToken = () => AsyncStorage.getItem(AUTH_TOKEN);
-export const setToken = token => AsyncStorage.setItem(AUTH_TOKEN, token);
-export const deleteToken = () => AsyncStorage.removeItem(AUTH_TOKEN);
