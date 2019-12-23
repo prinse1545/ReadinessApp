2a90f326f2d6a63c07e5d6319053ab0e35e602f3
prinse1545 <philipp.msrivastava@gmail.com>
2019-12-23 17:19:13 -0600

    soreness be done

--- focis/app/components/sorenessCard/index.js
+++ focis/app/components/sorenessCard/index.js
@@ -6,10 +6,11 @@ import {
   StyleSheet
 } from 'react-native';
 
-const SorenessCard = (props) => {
+const SorenessCard = ({bodyPart, soreness}) => {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text style={styles.text}>Body Part: {bodyPart}</Text>
+      <Text style={styles.text}>Soreness Level: {soreness}</Text>
     </View>
   )
 }
@@ -18,7 +19,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 10,
+    height: 70,
+    backgroundColor: '#035096'
+  },
+  text: {
+    fontFamily: 'Avenir',
+    color: '#fff',
+    fontSize: 18
   }
 })
 
--- focis/app/screens/internalScreens/questionScreens/soreness.js
+++ focis/app/screens/internalScreens/questionScreens/soreness.js
@@ -5,35 +5,96 @@ import {
   Text,
   StyleSheet,
   ScrollView,
-  Picker
+  Picker,
+  FlatList
 } from 'react-native';
-
-import { Button } from 'react-native-elements';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Slider } from 'react-native-elements';
+import AwesomeAlert from 'react-native-awesome-alerts';
 import SorenessCard from '../../../components/sorenessCard';
+import { setSoreness } from '../../../actions/day';
 
 const Soreness = ({navigation}) => {
 
-  const [parts, addPart] = useState([]);
+  const sorenessList = useSelector(state => state.day.soreness)
+  const dispatch = useDispatch()
+  const [bodyPart, setPart] = useState(null)
+  const [soreness, setSorenessLevel] = useState(null)
+  const [alert, toggleAlert] = useState(false)
 
   const addBodyPart = () => {
-    addPart([...parts, {id: parts.length, bodyPart: "", soreness: null}])
+    if(bodyPart == null || soreness == null) {
+      toggleAlert(true)
+      return
+    }
+
+    setSorenessLevel(null)
+    setPart(null)
+    dispatch(setSoreness([...sorenessList, {bodyPart: bodyPart, sorenessLevel: soreness}]))
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
+    <View>
+      <View style={styles.topContainer}>
+        <Text style={styles.instruction}>
+          Select your the body part, level of soreness, and click add soreness
+          to report your soreness level.
+        </Text>
+        <Picker
+          selectedValue={bodyPart}
+          style={{height: 50, width: 150, top: 30, position: 'absolute'}}
+          itemStyle={{color: '#fff'}}
+          onValueChange={(itemValue, itemIndex) =>
+            setPart(itemValue)
+          }>
+          <Picker.Item label="Feet" value="Feet" />
+          <Picker.Item label="Calves" value="Calves" />
+          <Picker.Item label="Hamstring" value="Hamstring" />
+          <Picker.Item label="Quadracepts" value="Quadracepts" />
+          <Picker.Item label="Abdomen" value="Abdomen" />
+          <Picker.Item label="Pectorals" value="Pectorals" />
+          <Picker.Item label="Lats" value="Lats" />
+          <Picker.Item label="Traps" value="Traps" />
+          <Picker.Item label="Lower Back" value="Lower Back" />
+        </Picker>
+        <View style={styles.sliderContainer}>
+          <Slider
+           maximumValue={5}
+           minimumValue={1}
+           value={soreness}
+           onValueChange={(value) => setSorenessLevel(value)}
+           step={1}
+           thumbTintColor='#fff'
+           style={styles.slider}
+          />
+          <Text style={styles.sliderValue}>{soreness}</Text>
+        </View>
         <Button
          title="Add Soreness"
-         buttonStyle={styles.addSorenessButton}
+         buttonStyle={styles.addButton}
          onPress={() => addBodyPart()}
         />
       </View>
-      {
-        parts.map((part) => {
-          <SorenessCard />
-        })
-      }
+      <FlatList
+        data={sorenessList}
+        renderItem={({item}) => <SorenessCard bodyPart={item.bodyPart} soreness={item.sorenessLevel} /> }
+        ListFooterComponent={<Button title="Finish" onPress={() => navigation.goBack()} />}
+      />
+      <AwesomeAlert
+        show={alert}
+        showProgress={false}
+        title="Invalid Input"
+        message="Please fill out fields before adding soreness!"
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showCancelButton={false}
+        showConfirmButton={true}
+        confirmText="Ok"
+        confirmButtonColor="#f00"
+        onConfirmPressed={() => {
+          toggleAlert(false)
+        }}
+      />
     </View>
   )
 }
@@ -47,8 +108,37 @@ const styles = StyleSheet.create({
   addSorenessButton: {
     borderRadius: 10
   },
-  inputContainer: {
-    flexDirection: 'row'
+  sliderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '30%'
+  },
+  sliderValue: {
+    fontFamily: 'Avenir',
+    fontSize: 16,
+    color: '#fff',
+    marginLeft: 5
+  },
+  slider: {
+    width: 250,
+  },
+  instruction: {
+    fontFamily: 'Avenir',
+    fontSize: 16,
+    color: '#fff',
+    width: '80%',
+    textAlign: 'center',
+    marginTop: 40
+  },
+  topContainer: {
+    alignItems: 'center',
+    backgroundColor: '#035096',
+    paddingBottom: 50
+  },
+  addButton: {
+    borderRadius: 10,
+    marginTop: 20
   }
 })
 
